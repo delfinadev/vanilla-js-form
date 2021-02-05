667cd933d57bd28647326f5ba2d27711caa9d4f5
delfinadev <delfinalasserre@gmail.com>
2021-02-05 17:52:06 -0300

    navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,16 +1,29 @@
-<<<<<<< HEAD
 
-import "./Navbar.css";
+import {FaHome} from 'react-icons/fa';
+import {FaUser} from 'react-icons/fa';
+import "../Components/Navbar.css";
 
 
 export default function Navbar(){
     return (
 <div className="navbar">
+    <img src="https://tuwebonline.co/wp-content/uploads/2020/05/logoepayco.png" alt="epayco"></img> <br></br>
+    <p className="user">ALEXANDER CEBA...</p>
+<p><FaHome/> Dashboard</p> <br></br>
+  <p><FaUser/> Clientes</p>
     </div>
 
     )
 }
 
+const Bare = () => {
+    return (
+      <div>
+        <FaHome/>
+        <FaUser/>
+      </div>
+    )
+  }
 
 
 
@@ -19,8 +32,4 @@ export default function Navbar(){
 
 
 
-=======
-export default function Navbar(params) {
-  return <div></div>;
-}
->>>>>>> 55abf6f7c3bfab01ceaf198cb0caa50fa501e858
+
