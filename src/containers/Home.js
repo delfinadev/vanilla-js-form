09a6225ffdf25a2fb9be553e77dc74504ff2a85c
gus <gustavo.segovia03@gmail.com>
2021-02-05 18:57:43 -0300

    tilosdando es

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,9 +1,17 @@
 import Form from '../Components/Form'
+import './Home.css'
 
 const Home = () => {
     return (
-        <div>
+        <div className="home">
+            <h2>Informacion del formulario</h2>
+            <span className="instructions">Ingrese el titulo y la descripción que visualizarán los usuarios durante el proceso de pago</span>
             <Form />
+            <footer>
+                <span>ePayco © 2011 - 2019 todos los derechos reservados. </span>
+                <img src="" alt=""/>
+                <img src="" alt=""/>
+            </footer>
         </div>
     )
 }
