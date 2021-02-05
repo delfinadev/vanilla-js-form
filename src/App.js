556d8d283b95423260e9254ac120c7d6afb1b4a0
gus <gustavo.segovia03@gmail.com>
2021-02-05 13:35:37 -0300

    Estructura realizada

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
-
+import Navbar from './Components/Navbar'
+import Home from './containers/Home'
+import swal from 'sweetalert'
 function App() {
   return (
     <div className="App">
-      
+      <Navbar />
+      <Home />
     </div>
   );
 }
