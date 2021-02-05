69fa4ea96711160149debce2a8d4faa15042ffcb
delfinadev <delfinalasserre@gmail.com>
2021-02-05 19:02:39 -0300

    css update

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -9,7 +9,7 @@ export default function Navbar() {
         src="https://tuwebonline.co/wp-content/uploads/2020/05/logoepayco.png"
         alt="epayco"></img>{" "}
       <br></br>
-      <p className="user">ALEXANDER CEBA...</p>
+      <p className="user"> ALEXANDER CEBA...</p>
       <p>
         <FaHome /> Dashboard
       </p>{" "}
